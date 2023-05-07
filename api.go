671bc7166d92afe78fc4a511bad7aef6630f00b1
Hakim Chulan <hakimchulan@gmail.com>
2023-05-07 08:48:49 +0800

    @22:23 Part 1

--- api.go
+++ api.go
@@ -80,7 +80,8 @@ func (s *APIServer) handleAccount (w http.ResponseWriter, r *http.Request) error
 }
 
 func (s *APIServer) handleGetAccount (w http.ResponseWriter, r *http.Request) error {
-	return nil
+	account := NewAccount("Hakim", "Chulan")
+	return WriteJSON(w, http.StatusOK, account)
 }
 
 func (s *APIServer) handleCreateAccount (w http.ResponseWriter, r *http.Request) error {
