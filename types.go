ef1f929f97579d647783434b283029303ef7bbbd
Hakim Chulan <hakimchulan@gmail.com>
2023-05-07 09:27:24 +0800

    when encoding json no space

--- types.go
+++ types.go
@@ -3,11 +3,11 @@ package main
 import "math/rand"
 
 type Account struct {
-	ID 			int 	`json; "id"`
-	FirstName 	string 	`json; "firstName"`
-	LastName 	string 	`json; "lastName"`
-	Number 		int64 	`json; "number"`
-	Balance 	int64 	`json; "balance"`
+	ID 			int 	`json:"id"`
+	FirstName 	string 	`json:"firstName"`
+	LastName 	string 	`json:"lastName"`
+	Number 		int64 	`json:"number"`
+	Balance 	int64 	`json:"balance"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
